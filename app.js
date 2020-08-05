5b458587c1ac0c0f5e94d57c35f2f7a638154c08
Mourad Bougarne <contact@mbougarne.me>
2020-08-05 15:39:53 +0100

    Add routes to entry point file

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const cors = require('cors')
 
 require('dotenv').config()
 
+const LoginRoutes = require('./routes/auth')
+const UsersRoutes = require('./routes/users')
+
 const app = express()
 
 let corsOptions = {}
@@ -10,5 +13,27 @@ let corsOptions = {}
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+});
+
+app.use('/api/auth', LoginRoutes)
+app.use('/api/test', UsersRoutes)
+
+// Show 404 on other routes
+app.use((req, res) => {
+    res.status(404).json({
+        status: 404,
+        succuss: false,
+        path: req.path,
+        URI: req.url,
+        message: 'Requested resource not found!'
+    })
+})
 
 module.exports = app
